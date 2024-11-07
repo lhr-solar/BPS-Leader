15d57d619701db58ade238208fa670d0111d54f9
Lakshay983 <lakshay45gupta@gmail.com>
2024-11-06 20:01:13 -0600

    started task stuff

--- Apps/Src/main.c
+++ Apps/Src/main.c
@@ -1,4 +1,14 @@
 // place holder for main.c
+#include "FreeRTOS.h"
+
+
 int main() {
+
+    
+    // start the scheduler
+    while(1){
+        // Scheduler should've started by now
+        // Code should never enter this point
+    }
     return 0;
 }
