ef1ee59760a3dee956f3531c084e0127d0e6cadc
Lakshay983 <lakshay45gupta@gmail.com>
2024-11-13 17:20:53 -0600

    the OS is not OSing

--- Apps/Src/main.c
+++ Apps/Src/main.c
@@ -9,27 +9,46 @@
 #include "Tasks.h"
 #include "LEDs.h"
 
+StaticTask_t pxTaskBufferRx;
+
 int main() {
-    //Heartbeat_Init();
+    Heartbeat_Init();
+    Heartbeat_Toggle();
+
+
+    /* Create the queue receive task as described in the comments at the top
+    of this file. */
+    xTaskCreateStatic( /* The function that implements the task. */
+                    Task_Init,
+                    /* Text name for the task, just to help debugging. */
+                    "Init Task",
+                    /* The size (in words) of the stack that should be created
+                    for the task. */
+                    configMINIMAL_STACK_SIZE,
+                 /* A parameter that can be passed into the task. Not used
+                    in this simple demo. */
+                    NULL,
+                 /* The priority to assign to the task. tskIDLE_PRIORITY
+                    (which is 0) is the lowest priority. configMAX_PRIORITIES - 1
+                    is the highest priority. */
+                    TASK_INIT_PRIO,
+                 /* Used to obtain a handle to the created task. Not used in
+                    this simple demo, so set to NULL. */
+                    NULL,
+                    /* Buffer for static allocation */
+                    &pxTaskBufferRx );
+
     //Heartbeat_Toggle();
-
-   xTaskCreateStatic(
-      Task_Init,            /* Function that implements the task. */
-      "Task_Init",          /* Text name for the task. */
-      TASK_INIT_STACK_SIZE, /* Stack size in words. */
-      NULL,                 /* Parameter passed into the task. */
-      TASK_INIT_PRIO,       /* Task Priority. */
-      NULL,                  /* Task Handle. */
-      NULL );
-
+    //Heartbeat_Toggle();
     // Start the scheduler
     vTaskStartScheduler();
-    
-    
+
     // start the scheduler
     while(1){
+        //Heartbeat_Toggle();
         // Scheduler should've started by now
         // Code should never enter this point
+        HAL_Delay(100);
     }
     
     return 0;
--- Tasks/Src/InitTask.c
+++ Tasks/Src/InitTask.c
@@ -2,27 +2,8 @@
 #include "LEDs.h"
 
 void Task_Init(){
-    Heartbeat_Init();
-    Heartbeat_Toggle();
-
-    xTaskCreateStatic(
-      Task_Init,                        /* Function that implements the task. */
-      "Task_Voltage_Monitor",           /* Text name for the task. */
-      TASK_VOLTAGE_MONITOR_STACK_SIZE,  /* Stack size in words. */
-      NULL,                             /* Parameter passed into the task. */
-      TASK_VOLTAGE_MONITOR_PRIO,        /* Task Priority. */
-      NULL,                             /* Task Handle. */
-      NULL );
-
-    xTaskCreateStatic(
-      Task_Init,                            /* Function that implements the task. */
-      "Task_Temperature_Monitor",           /* Text name for the task. */
-      TASK_TEMPERATURE_MONITOR_STACK_SIZE,  /* Stack size in words. */
-      NULL,                                 /* Parameter passed into the task. */
-      TASK_TEMPERATURE_MONITOR_PRIO,        /* Task Priority. */
-      NULL,                                 /* Task Handle. */
-      NULL );
-
+    //Heartbeat_Init();
+    //Heartbeat_Toggle();
 
     // Task deletes itself when all other tasks are Init'd
     vTaskDelete(NULL);
