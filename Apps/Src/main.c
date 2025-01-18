1210ce40a824fee4ee861c6c0b8df108522c414f
Lakshay983 <lakshay45gupta@gmail.com>
2025-01-18 13:12:52 -0600

    moved stack array

--- Apps/Src/main.c
+++ Apps/Src/main.c
@@ -4,19 +4,19 @@
 
 
 int main() {
-    xTaskCreateStatic(
-                    Task_Init, /* The function that implements the task. */
-                    "Init Task", /* Text name for the task. */
-                    configMINIMAL_STACK_SIZE, /* The size (in words) of the stack that should be created for the task. */
-                    (void*)NULL, /* Paramter passed into the task. */
-                    TASK_INIT_PRIO, /* Task Prioriy. */
-                    Task_Init_Stack_Array, /* Stack array. */
-                    &Task_Init_Buffer  /* Buffer for static allocation. */
-   );
+//     xTaskCreateStatic(
+//                     Task_Init, /* The function that implements the task. */
+//                     "Init Task", /* Text name for the task. */
+//                     configMINIMAL_STACK_SIZE, /* The size (in words) of the stack that should be created for the task. */
+//                     (void*)NULL, /* Paramter passed into the task. */
+//                     TASK_INIT_PRIO, /* Task Prioriy. */
+//                     Task_Init_Stack_Array, /* Stack array. */
+//                     &Task_Init_Buffer  /* Buffer for static allocation. */
+//    );
 
 
-    // Start the scheduler
-    vTaskStartScheduler();
+//     // Start the scheduler
+//     vTaskStartScheduler();
 
     while(1){
         // Scheduler should've started by now
--- Tasks/Inc/BPS_Tasks.h
+++ Tasks/Inc/BPS_Tasks.h
@@ -1,6 +1,5 @@
 #ifndef TASK_H__
 #define TASK_H__
-
 /* Kernel includes. */
 #include "FreeRTOS.h" /* Must come first. */
 #include "task.h" /* RTOS task related API prototypes. */
@@ -8,7 +7,7 @@
 #include "timers.h" /* Software timer related API prototypes. */
 #include "semphr.h" /* Semaphore related API prototypes. */
 
-#include "stm32xx_hal.h"
+// #include "stm32xx_hal.h"
 
 // Task Priority 
 #define TASK_INIT_PRIO                      tskIDLE_PRIORITY + 2
@@ -24,20 +23,6 @@
 #define TASK_AMPERES_MONITOR_STACK_SIZE            configMINIMAL_STACK_SIZE
 #define TASK_PETWDOG_STACK_SIZE                    configMINIMAL_STACK_SIZE
 
-
-// Task Stack Arrays
-StackType_t Task_Init_Stack_Array[ configMINIMAL_STACK_SIZE ];
-StackType_t Task_Temperature_Stack_Array[ configMINIMAL_STACK_SIZE ];
-StackType_t Task_Voltage_Stack_Array[ configMINIMAL_STACK_SIZE ];
-StackType_t Task_Amperes_Stack_Array[ configMINIMAL_STACK_SIZE ];
-StackType_t Task_Petwdog_Stack_Array[ configMINIMAL_STACK_SIZE ];
-
-// Task Buffers
-StaticTask_t Task_Init_Buffer;
-StaticTask_t Task_Temperature_Buffer;
-StaticTask_t Task_Voltage_Buffer;
-StaticTask_t Task_Amperes_Buffer;
-StaticTask_t Task_Petwdog_Buffer;
 
 // Task Inits
 void Task_Init();
