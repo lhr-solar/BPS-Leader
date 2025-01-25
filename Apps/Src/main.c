d441f296bd8358e4da7f0ee85e22abf57036977d
Lakshay983 <lakshay45gupta@gmail.com>
2025-01-25 14:31:07 -0600

    make sure works on hw

--- Apps/Src/main.c
+++ Apps/Src/main.c
@@ -32,21 +32,6 @@ int main() {
                     Task_Init_Stack_Array, /* Stack array. */
                     &Task_Init_Buffer  /* Buffer for static allocation. */
    );
-
-       GPIO_InitTypeDef led_config = {
-        .Mode = GPIO_MODE_OUTPUT_PP,
-        .Pull = GPIO_NOPULL,
-        .Pin = HEARTBEATPIN
-    };
-    
-    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
-    HAL_GPIO_Init(GPIOA, &led_config); // initialize GPIOA with led_config
-
-    while(1){
-        HAL_GPIO_TogglePin(HEARTBEATPORT, HEARTBEATPIN);
-        HAL_Delay(100);
-    }
-
     // Start the scheduler
     vTaskStartScheduler();
 
--- Tasks/Src/InitTask.c
+++ Tasks/Src/InitTask.c
@@ -4,20 +4,20 @@
 
 void Task_Init(){
 
-    // Task deletes itself when all other tasks are Init'd
-    // GPIO_InitTypeDef led_config = {
-    //     .Mode = GPIO_MODE_OUTPUT_PP,
-    //     .Pull = GPIO_NOPULL,
-    //     .Pin = HEARTBEATPIN
-    // };
+    //Task deletes itself when all other tasks are Init'd
+    GPIO_InitTypeDef led_config = {
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Pin = HEARTBEATPIN
+    };
     
-    // __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
-    // HAL_GPIO_Init(GPIOA, &led_config); // initialize GPIOA with led_config
+    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
+    HAL_GPIO_Init(GPIOA, &led_config); // initialize GPIOA with led_config
 
-    // while(1){
-    //     HAL_GPIO_TogglePin(HEARTBEATPORT, HEARTBEATPIN);
-    //     HAL_Delay(100);
-    // }
+    while(1){
+        HAL_GPIO_TogglePin(HEARTBEATPORT, HEARTBEATPIN);
+        HAL_Delay(500);
+    }
 
 
     vTaskDelete(NULL);
