8a16a118f1ffe689d9a666c5c32a541bb608eb3f
Lakshay983 <lakshay45gupta@gmail.com>
2025-02-01 11:00:29 -0600

    add some topics

--- Drivers/Src/LEDs.c
+++ Drivers/Src/LEDs.c
@@ -1,8 +1,6 @@
 #include "LEDs.h"
 
-#define CLOCK_INIT(port) __HAL_RCC_##port##_CLK_ENABLE();
-
-/**
+/**`
  * Inits the Heartbeat pin based on HEARTBEATPIN and HEARTBEATPORT
  * From PinConfig.h
 */
@@ -17,13 +15,33 @@ void Heartbeat_Init(){
 
 }
 
+/**
+ * Inits the GPIO port clock based on HEARTBEATPORT
+ * Can be called multiple times
+ */
 void Heartbeat_Clock_Init(){
-    CLOCK_INIT(HEARTBEATPORT);
+    
+    if(HEARTBEATPORT == GPIOA){
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+    }
+    if(HEARTBEATPORT == GPIOB){
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+    }
+    if(HEARTBEATPORT == GPIOC){
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+    }
+    if(HEARTBEATPORT == GPIOD){
+        __HAL_RCC_GPIOD_CLK_ENABLE();
+    }
+    if(HEARTBEATPORT == GPIOE){
+        __HAL_RCC_GPIOE_CLK_ENABLE();
+    }
 }
 
 void Heartbeat_Toggle(){
     HAL_GPIO_TogglePin(HEARTBEATPORT, HEARTBEATPIN);
 }
+
 
 void Heartbeat_WritePin(uint8_t new_val){
     HAL_GPIO_WritePin(HEARTBEATPORT, HEARTBEATPIN, (new_val == GPIO_PIN_SET) ? GPIO_PIN_SET: GPIO_PIN_RESET);    
