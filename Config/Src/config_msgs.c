2f12fc44e67cc05649d39eca39e39beecbe18ea7
Lakshay983 <lakshay45gupta@gmail.com>
2025-02-05 16:16:00 -0600

    remove pragma messages

--- Config/Src/config_msgs.c
+++ Config/Src/config_msgs.c
@@ -5,7 +5,3 @@
 #define PRE(s)  "\r    "s"  "   // \r removes the filepath and 'note: '#pragma message:...' parts
 #define STR(x)  #x
 #define XSTR(x) STR(x)
-
-#pragma message(PRE("⚡") "NUM_BATTERY_MODULES set to                " XSTR(NUM_BATTERY_MODULES))
-
-#pragma message(PRE("🔥") "NUM_TEMPERATURE_SENSORS set to            " XSTR(NUM_TEMPERATURE_SENSORS))
