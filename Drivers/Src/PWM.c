d0f0946bdd2f6b68c0d4605c68f6c28450e947ea
Phoenix Nguyen <phoenixn831@gmail.com>
2025-02-14 01:35:14 -0600

    switch case and fixed callback!

--- Drivers/Src/PWM.c
+++ Drivers/Src/PWM.c
@@ -4,13 +4,19 @@
 #define PWM_SEND_QUEUE_SIZE (10)
 #endif
 
+#ifndef PWM_INFO_SIZE 
+#define PWM_INFO_SIZE sizeof(PWM_Info)
+#endif
+
+BaseType_t higherPriorityTaskWoken = pdFALSE;
+
 static QueueHandle_t pwm1_send_queue = NULL;  // need to add queue for each PWM timer
 static StaticQueue_t pwm1_send_queue_buffer;
-static uint8_t pwm1_send_queue_storage[PWM_SEND_QUEUE_SIZE*sizeof(PWM_Info)];
+static uint8_t pwm1_send_queue_storage[PWM_SEND_QUEUE_SIZE*PWM_INFO_SIZE];
         
 static QueueHandle_t pwm2_send_queue = NULL;  // need to add queue for each PWM timer
 static StaticQueue_t pwm2_send_queue_buffer;
-static uint8_t pwm2_send_queue_storage[PWM_SEND_QUEUE_SIZE*sizeof(PWM_Info)];
+static uint8_t pwm2_send_queue_storage[PWM_SEND_QUEUE_SIZE*PWM_INFO_SIZE];
 
 static TIM_OC_InitTypeDef sConfigOC = {0};
 
@@ -58,6 +64,17 @@ void MX_GPIO_Init(){
     HAL_GPIO_Init(GPIOA, &pwm_tim9_ch1);
 }
 
+QueueHandle_t* BSP_PWM_Get_Queue(TIM_HandleTypeDef* timHandle) {
+    switch((uint32_t)timHandle->Instance) {
+        case (uint32_t)TIM1:
+            return &pwm1_send_queue;
+        case (uint32_t)TIM9:
+            return &pwm2_send_queue;
+        //add more timers later maybe
+    }
+    return 0;
+}
+
 HAL_StatusTypeDef BSP_PWM_TIM_Init(TIM_HandleTypeDef* timHandle) {
     HAL_StatusTypeDef stat = HAL_OK;
     
@@ -65,21 +82,20 @@ HAL_StatusTypeDef BSP_PWM_TIM_Init(TIM_HandleTypeDef* timHandle) {
          __HAL_RCC_GPIOA_CLK_ENABLE();
     
     if (timHandle->Instance == TIM1 && pwm1_send_queue == NULL) {// assign queue for specific timer
-
-        pwm1_send_queue = xQueueCreateStatic(PWM_SEND_QUEUE_SIZE, sizeof(PWM_Info),
+        pwm1_send_queue = xQueueCreateStatic(PWM_SEND_QUEUE_SIZE, PWM_INFO_SIZE,
         pwm1_send_queue_storage, &pwm1_send_queue_buffer);
     
         if (pwm1_send_queue == NULL) 
             return HAL_ERROR; 
-        }
+    }
     else if (timHandle->Instance == TIM9 && pwm2_send_queue == NULL) {
         
-        pwm2_send_queue = xQueueCreateStatic(PWM_SEND_QUEUE_SIZE, sizeof(PWM_Info),
+        pwm2_send_queue = xQueueCreateStatic(PWM_SEND_QUEUE_SIZE, PWM_INFO_SIZE,
         pwm2_send_queue_storage, &pwm2_send_queue_buffer); 
 
         if (pwm2_send_queue == NULL) 
             return HAL_ERROR; 
-        }
+    }
 
     stat = HAL_TIM_PWM_Init(timHandle);
     if(stat == HAL_ERROR) return stat;
@@ -95,10 +111,8 @@ HAL_StatusTypeDef BSP_PWM_Channel_Init(TIM_HandleTypeDef* timHandle, uint8_t cha
     sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
     sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
 
-    
     stat = HAL_TIM_PWM_ConfigChannel(timHandle, &sConfigOC, channel);
     if(stat == HAL_ERROR) return stat;
-    // while(timHandle->Channel[])
 
     stat = HAL_TIM_PWM_Start_IT(timHandle, channel); 
     return stat;
@@ -107,11 +121,7 @@ HAL_StatusTypeDef BSP_PWM_Channel_Init(TIM_HandleTypeDef* timHandle, uint8_t cha
 HAL_StatusTypeDef BSP_PWM_Set(TIM_HandleTypeDef* timHandle, uint8_t channel, uint8_t dutyCycle, uint64_t speed) {
     if(dutyCycle <= 100 && dutyCycle >= 0) {
 
-        QueueHandle_t tx_Queue = NULL;
-        if (timHandle->Instance == TIM1) // get queue for specific timer
-            tx_Queue = pwm1_send_queue;
-        else if (timHandle->Instance == TIM9)
-            tx_Queue = pwm2_send_queue;
+        QueueHandle_t* tx_Queue = BSP_PWM_Get_Queue(timHandle);
     
         PWM_Info pwmSend = { // for storing PWM info into queue
             .timHandle = timHandle,
@@ -120,11 +130,7 @@ HAL_StatusTypeDef BSP_PWM_Set(TIM_HandleTypeDef* timHandle, uint8_t channel, uin
             .speed = speed
         };
 
-        // if (sConfigOC.Pulse != dutyCycle*(timHandle->Init.Period)/100) {
-            
-        // }
-
-        if(!xQueueSend(tx_Queue, &pwmSend, 0)) {
+        if(!xQueueSend(*tx_Queue, &pwmSend, 0)) {
             return HAL_ERROR;
         }
             
@@ -133,24 +139,20 @@ HAL_StatusTypeDef BSP_PWM_Set(TIM_HandleTypeDef* timHandle, uint8_t channel, uin
     return HAL_ERROR;
 }
 
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *timHandle) {
+void HAL_TIM_PWM_PeriodElapsedCallback(TIM_HandleTypeDef *timHandle) {
 
     // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5); // testing interrupt callback
-    QueueHandle_t tx_Queue = NULL;
-    
-    if (timHandle->Instance == TIM1)
-        tx_Queue = pwm1_send_queue;
-    else if (timHandle->Instance == TIM9)
-        tx_Queue = pwm2_send_queue;
+    QueueHandle_t* tx_Queue = BSP_PWM_Get_Queue(timHandle);
  
 
-    if(!xQueueIsQueueEmptyFromISR(tx_Queue)) {
+    if(!xQueueIsQueueEmptyFromISR(*tx_Queue)) {
         PWM_Info pwmReceive; 
-        xQueueReceiveFromISR(tx_Queue, &pwmReceive, 0);
+        xQueueReceiveFromISR(*tx_Queue, &pwmReceive, &higherPriorityTaskWoken);
         HAL_TIM_PWM_Stop_IT(pwmReceive.timHandle, pwmReceive.channel);
         // pwmReceive.timHandle->Init.Period = pwmReceive.speed;
         sConfigOC.Pulse = pwmReceive.dutyCycle*(pwmReceive.timHandle->Init.Period)/100;
         HAL_TIM_PWM_ConfigChannel(pwmReceive.timHandle, &sConfigOC, pwmReceive.channel);
         HAL_TIM_PWM_Start_IT(pwmReceive.timHandle, pwmReceive.channel);
     }
+    portYIELD_FROM_ISR(higherPriorityTaskWoken);
 }
